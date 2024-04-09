f814ddc3e719251a7de68bab928bdff38700bf6b
wgarcia25 <willy.garcia25@icloud.com>
2024-04-08 22:03:18 -0700

    Finished HW 8

---
+++ homeworks/hw8/assets/js/script.js
@@ -0,0 +1,6 @@
+function toggleSongs () {
+    let songsSecondHalf = document.getElementById("songs-second-half");
+    songsSecondHalf.classList.toggle("hidden");
+}
+
+document.getElementById("toggle-button").onclick = toggleSongs;
