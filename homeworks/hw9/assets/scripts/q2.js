57adc01b283a748a1645c6fb90c84e0ba88c3f79
wgarcia25 <willy.garcia25@icloud.com>
2024-04-18 00:23:17 -0700

    Finished HW 9

--- homeworks/hw9/assets/scripts/q2.js
+++ homeworks/hw9/assets/scripts/q2.js
@@ -12,8 +12,10 @@ function unhideLightbox(lightboxID) {
 	document.getElementById('lightbox-overlay').classList.remove('hidden');
 
 	// TODO: Remove the .hidden class from the div with the given id
-	document.getElementById().classList.remove('hidden');
+	document.getElementById(lightboxID).classList.remove('hidden');
+
 }
+
 
 
 //
@@ -25,16 +27,27 @@ function unhideLightbox(lightboxID) {
  */
 function unhideLightbox1() {
 	// TODO: Look in q2.html to see what the id for the lightbox div for the first picture, and call unhideLightbox
-	unhideLightbox("#doggo1");
+	unhideLightbox("doggo1");
 }
 
-document.getElementsById("#picture-1").onclick = unhideLightbox1;
+document.getElementById("picture-1").onclick = unhideLightbox1;
 
 // TODO: Set the browser to run the function unhideLightbox1 when someone clicks #picture-1
 
 
+
+
+
+
+
 // TODO: do the same for #picture-2, write a function and then make it run on click
 
+function unhideLightbox2() {
+	// TODO: Look in q2.html to see what the id for the lightbox div for the first picture, and call unhideLightbox
+	unhideLightbox("doggo2");
+}
+
+document.getElementById("picture-2").onclick = unhideLightbox2;
 
 
 
@@ -42,7 +55,12 @@ document.getElementsById("#picture-1").onclick = unhideLightbox1;
 // TODO: do the same for #picture-3, write a function and then make it run on click
 
 
+function unhideLightbox3() {
+	// TODO: Look in q2.html to see what the id for the lightbox div for the first picture, and call unhideLightbox
+	unhideLightbox("doggo3");
+}
 
+document.getElementById("picture-3").onclick = unhideLightbox3;
 
 
 
@@ -54,7 +72,7 @@ function closeLightbox(lightboxID) {
 	document.getElementById('lightbox-overlay').classList.add('hidden');
 
 	// TODO: Add the .hidden class to the div with the given id
-
+	document.getElementById(lightboxID).classList.add('hidden');
 }
 
 
@@ -73,3 +91,6 @@ function closeAllLightboxes() {
 }
 
 // TODO: set the closeAllLightboxes function to run when #lightbox-overlay is clicked on.
+
+
+document.getElementById("lightbox-overlay").onclick = closeAllLightboxes;
